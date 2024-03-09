fa51f4871edeafd284a5e7bb68df92dc8667deb5
Simo672K <mohammed.hakmiii555@gmail.com>
2024-03-10 00:31:21 +0100

    channels with goroutines

--- channels.go
+++ channels.go
@@ -1,12 +1,58 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
 
-func HeavyFunction() string {
+type ChannelServer struct {
+	quitch chan struct {}
+	msgch chan string
+}
 
+
+func (s *ChannelServer) FetchData() string {
+	time.Sleep(time.Second * 4)
 	return "return value"
 }
 
-func main() {
-	
+func (s *ChannelServer) CreateMessage(msg string) {
+	s.msgch <- msg
 }
 
+func NewServer() *ChannelServer {
+	return &ChannelServer{
+		msgch: make(chan string),
+	}
+}
+
+func (s *ChannelServer) HandleMessage(message string) {
+	fmt.Println("message received:", message)
+}
+
+func (s *ChannelServer) Start() {
+	s.loop()
+}
+
+func (s *ChannelServer) loop() {
+	mainloop:for {
+		select {
+		case <- s.quitch:
+			break mainloop
+		case msg:= <-s.msgch:
+			s.HandleMessage(msg)
+		default:
+		}
+	}
+}
+
+
+func ChannelMainFunc () string {
+	server := NewServer()
+	go server.Start()
+	
+	server.CreateMessage("Hello this is an other message in message channel!")
+	time.Sleep(time.Second * 10)
+
+	return "Done"
+}
